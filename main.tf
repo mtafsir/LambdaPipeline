dcff6c16f45b36f6f6cfe9d5c6004c70af54725a
mtafsir <49006769+mtafsir@users.noreply.github.com>
2023-09-04 19:53:50 -0500

    add ec2 example

--- main.tf
+++ main.tf
@@ -17,3 +17,32 @@ terraform {
 
   required_version = ">= 0.14.0"
 }
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
